d65629e431bd6efb6c4c76801658d87f8a28d60e
Piyush kushwah <piyushkushwah002@gmail.com>
2026-04-02 16:59:08 +0530

    Add files via upload

---
+++ DSA - Practice/src/com/piyush/StableSort.java
@@ -0,0 +1,29 @@
+package com.piyush;
+
+import java.util.*;
+
+public class StableSort {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 4, 2, 2, 1 };
+
+		for (int i = 1; i < arr.length; i++) {
+			int key = arr[i];
+			int j = i - 1;
+
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+
+			arr[j + 1] = key;
+		}
+
+		for (int x : arr) {
+			System.out.print(x + " ");
+
+		}
+	}
+
+}
