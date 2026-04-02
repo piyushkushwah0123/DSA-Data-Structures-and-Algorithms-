90188e081c09d517478a781dd46dd8d1e09be8ae
Piyush kushwah <piyushkushwah002@gmail.com>
2026-04-02 16:58:25 +0530

    Add files via upload

---
+++ DSA - Practice/src/com/piyush/SortString.java
@@ -0,0 +1,16 @@
+package com.piyush;
+
+import java.util.*;
+
+public class SortString {
+	
+	    public static void main(String[] args) {
+	        char[] arr = {'a','b','c','d','j','a','b'};
+
+	        Arrays.sort(arr);
+
+	        System.out.println(Arrays.toString(arr));
+	    
+	}
+
+}
